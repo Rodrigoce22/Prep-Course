185b1157b80105a2739d1abc76f3497209e6cec6
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-21 21:07:35 -0300

    .

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -161,7 +161,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return (nombre, apellido).split('');
+  return combinarNombres.split("");
 }
 
 function obtenerSaludo(nombre) {
