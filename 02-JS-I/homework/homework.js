6b81774692759c2a4d9dd1395210525f3411ac99
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-23 18:39:24 -0300

    .

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -190,7 +190,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  let obtenerSaludo = "Hola" + ("")+nombre;
+  let obtenerSaludo = "Hola" + (" ") + nombre;
   return obtenerSaludo;
 }
 
