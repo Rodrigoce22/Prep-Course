0469c4d56efcda05a5c51b2e7c59552f9c306e3b
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-22 21:17:42 -0300

    .

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,9 +73,9 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (str1.length == str2.length)
-    return false;
-  else
     return true;
+  else
+    return false;
 }
 
 function menosQueNoventa(num) {
