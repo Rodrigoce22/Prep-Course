1909f26a099191de64095d735fa48eabb5d9216d
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-23 19:32:57 -0300

    .

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -230,7 +230,7 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra.length>1)
     return "Dato incorrecto"
-  if(letra==="a" || letra==="e"||letra==="i"||letra==="o"||letra==="u") return "Es vocal";
+  if(letra==="a" || letra==="e"||letra==="i"||letra==="o"||letra==="u") return "Es vocal"
   return "Dato incorrecto"
 }
 
