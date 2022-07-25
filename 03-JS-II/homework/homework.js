5dbc94f0ae4d52b0a3681331ca10be6d80062288
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-25 19:19:42 -0300

    .

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -50,9 +50,9 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
   if (idioma===("aleman")){
-  return "Guten Tag";}
+  return "Guten Tag!";}
   else if (idioma===("mandarin")){
-  return "Ni Hao";}
+  return "Ni Hao!";}
   else if(idioma===("ingles")){
   return "Hello!";}
   else{
