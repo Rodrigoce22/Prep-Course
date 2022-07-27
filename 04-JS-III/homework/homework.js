ca1e75b14daf1b45933d4f023d2f7e58bf63d54a
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-27 18:37:41 -0300

    .

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var narray=[];
+  for(var i=0; i<array.length;i++){
+    narray[i]=array[i]=1;}
+    return narray;
 }
 
 
