983c5db8659d371fc19853d28bd63501c5abb0e9
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-27 20:38:46 -0300

    .

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -66,11 +66,12 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  for(i=0;i==elemento;i++);
-  if(array[i]===(Elemento));{
-    return true}
-    else{
-    return false}
+  for(var i=0;i<array.length;i++);{
+  if(array[i]===elemento);{
+    return true;
+    }
+  }
+  return false;
 }
 
 
