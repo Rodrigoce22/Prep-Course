4e081ec9ea493c28a08f9d13bed1da4d75b8d4e0
Rodrigoce22 <cepedarodrigo52@gmail.com>
2022-07-28 18:07:20 -0300

    .

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -80,10 +80,10 @@ function agregarNumeros(numeros) {
   // Suma todos los enteros y devuelve el valor
   // Tu código:
   var suma=0;
-  for(var i=0;i<numeros.lenght;i++);{
-  suma=suma+numeros[i];}
-  {
-  return suma;}
+  for(var i=0;i<numeros.lenght;i++){
+  suma=suma+numeros[i];
+}
+  return suma;
 }
 
 
